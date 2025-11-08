929000f29f785603e61b94ecbb5a07b29b20908f
muckfaru <marklloren169@gmail.com>
2025-11-08 23:56:53 +0800

    fix: Remove LAN mode override in lobby to respect ForceNetworkMode

--- script/code_breaker_lobby.gd
+++ script/code_breaker_lobby.gd
@@ -471,8 +471,8 @@ func _initialize_multiplayer_config() -> void:
 	_multiplayer_config = MultiplayerConfigScript.new()
 	add_child(_multiplayer_config)
 	
-	# Set mode: LAN for same WiFi testing, LOCALHOST for PC-only dev
-	_multiplayer_config.set_mode(_multiplayer_config.Mode.LAN)
+	# Don't override mode here - let ForceNetworkMode autoload control it
+	# Mode is already set by ForceNetworkMode in project autoload
 	
 	# Get lobby server URL
 	_lobby_server_url = _multiplayer_config.get_lobby_url()
